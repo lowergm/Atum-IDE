7605671080d818d0d892431b36394603de910e96
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-19 22:28:50 -0300

    add: style

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var editor = ace.edit(document.getElementById("editor-container"));
 
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/twilight");
+
 
 editor.session.setMode("ace/mode/javascript");
 
